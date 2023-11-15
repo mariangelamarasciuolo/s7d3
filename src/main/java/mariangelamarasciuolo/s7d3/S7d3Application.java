c04b45f38087170d838475c9a21c6391924903da
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-11-15 15:13:08 +0100

    Initial commit

---
+++ src/main/java/mariangelamarasciuolo/s7d3/S7d3Application.java
@@ -0,0 +1,13 @@
+package mariangelamarasciuolo.s7d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7d3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7d3Application.class, args);
+	}
+
+}
---
+++ src/test/java/mariangelamarasciuolo/s7d3/S7d3ApplicationTests.java
@@ -0,0 +1,13 @@
+package mariangelamarasciuolo.s7d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S7d3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
